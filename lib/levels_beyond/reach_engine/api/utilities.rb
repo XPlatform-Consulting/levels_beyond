cea172fe61c9d3e53ef7d38dc42ba7a976eb06f9
John Whitson <john.whitson@gmail.com>
2014-04-14 20:30:42 -0600

    Removed search method stub Fixed asset_ingest_any method to use the new workflow_execution_start method name

--- lib/levels_beyond/reach_engine/api/utilities.rb
+++ lib/levels_beyond/reach_engine/api/utilities.rb
@@ -37,22 +37,11 @@ module LevelsBeyond
           watch_folder_disable(watch_folder_id)
         end
 
-        def search(args = { })
-          # [4/11/14 4:03:49 PM] Nicholas Stokes:
-          #   search-asset {"type":"video,project,image", {"name = 'LIKE'":"ubiquity", "SIZE": 100, "OFFSET": 0, "dataUpdated": "DESC"} }
-          #   172.16.180.189:8080/reachengine/api/v1/search?types=|ImageAssetMaster|Project|Timeline|&rql=name LIKE 'ubiquity' SIZE 100 OFFSET 0 ORDER_BY dateUpdated DESC&apiKey=6c6f19c1-73d1-40f6-8ad9-64878ca5879c
-          #   search-asset {"type":"video|project|image", {"name = 'LIKE'":"ubiquity", "SIZE": 100, "OFFSET": 0, "dataUpdated": "DESC"} }
-          super(args)
-        end
         alias :search_asset :search
 
 
         def asset_ingest_any(args = { })
-          parameters = cached_method_parameters[__method__] ||= [
-            { :name => :file_to_ingest, :required => true },
-            #{ :name => :workflow_id,    :default => default_asset_ingest_any_workflow_id },
-            :workflow_id
-          ]
+          parameters = api_method_parameters(__method__)
           _args = process_parameters(parameters, args.dup)
 
           workflow_id = _args.delete(:workflow_id) { default_asset_ingest_any_workflow_id }
@@ -60,9 +49,13 @@ module LevelsBeyond
 
           raise ArgumentError, ':file_to_ingest is a required argument.' unless file_to_ingest
 
-          workflow_start(workflow_id, { :context_data => { :file_to_ingest => file_to_ingest } }.merge(args))
+          workflow_execution_start(workflow_id, { :context_data => { :file_to_ingest => file_to_ingest } }.merge(args))
         end
         alias :create_asset :asset_ingest_any
+        API_METHOD_PARAMETERS[:asset_ingest_any] = [
+          { :name => :file_to_ingest, :required => true },
+          :workflow_id
+        ]
 
         def create_watch_folder_and_ingest_assets_into_collection(args = { })
           args[:workflow_key] ||= '_ingestAssetToCollection'
